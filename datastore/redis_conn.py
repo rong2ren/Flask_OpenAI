190a0d764041012ec24bdab9130cbee3027e3563
rong2ren <98350815+rong2ren@users.noreply.github.com>
2023-04-29 14:24:18 -0700

    rename redis_conn module

--- datastore/redis_conn.py
+++ datastore/redis_conn.py
@@ -1,103 +1,89 @@
-import uuid
 import redis
-import os  # for reading redis config
+from config import logger
+import os
 
 """
-Not used anymore! Use RedisConn.py instead!
+docker run -d --name redis-stack -p 6379:6379 -p 8001:8001 redis/redis-stack:latest
+localhost:8001
+docker ps
+docker stop redis-stack
+docker exec -it redis-stack redis-cli
 """
 
+class RedisClient:
+    def __init__(self):
+        # Initialize a Redis instance with decode_response set to True
+        self.redis_client = redis.Redis(
+            host=os.getenv('REDIS_HOST', 'localhost'),
+            port=os.getenv('REDIS_PORT', 6379),
+            db=0,
+            decode_responses=True
+        )
+        self.check_connection()
+        
+        to_remove_all = os.getenv('REDIS_REMOVE_ALL_WHEN_START')
+        if to_remove_all.lower() in ['true', '1']:
+            self.redis_client.flushall()
 
-def connect_to_redis():
-    redis_client = redis.Redis(
-        host=os.getenv('REDIS_HOST', 'localhost'),
-        port=os.getenv('REDIS_PORT', 6379),
-        db = 0,
-        decode_responses=True
-    )
-    if redis_client.ping():
-        redis_client.flushall() #remove all existing keys from Redis
-        return redis_client
-    else:
-        print("Redis: failed to connect to Redis")
-        return None
+    def get_redis_instance(self):
+        return self.redis_client
 
-def check_redis_connection(redis_client):
-    return redis_client.ping()
+    def check_connection(self):
+        # Check if the Redis client object is connected to the Redis cache
+        try:
+            self.redis_client.ping()
+            return True
+        except redis.exceptions.ConnectionError as e:
+            logger.error(f"Redis: connection failed. {e}")
+            return False
 
-def remove_all_from_redis(redis_client):
-    return redis_client.flushall()
-
-def generate_user_id():
-    # Generate a unique user ID.
-    """
-    #another way for user_id:
-    timestamp = int(time.time())
-    random_value = uuid.uuid4().hex[:6] # Generate a random hex string of length 6
-    return f"user_{timestamp}_{random_value}"
-    """
-    return str(uuid.uuid4())
-
-def add_1_book_to_redis(redis_client, user_id, book):
-    """
-    Add one books to Redis for the given user ID.
-    Returns num of books added, or raises an exception if the user ID is not provided.
-    """
-    if not user_id:
-        #raise ValueError("Redis: User ID not provided")
-        return 0
-    else:
-        return redis_client.sadd(user_id, book)
-
-def add_books_to_redis(redis_client, user_id, book_list):
-    """
-    Add a list of books to Redis for the given user ID.
-    Returns 1 if the operation was successful, or raises an exception if the user ID is not provided.
-    """
-    if not user_id:
-        #raise ValueError("Redis: User ID not provided")
-        return 0
-    else:
-        #num_of_books = 0
-        #for book in book_list:
-        #    num_of_books += redis_client.sadd(user_id, book)
-        #return num_of_books
-        return redis_client.sadd(user_id, *book_list)
+    def remove_all(self):
+        # Remove all data in the Redis cache
+        return self.redis_client.flushall()
     
-def get_number_of_books_for_userid(redis_client, user_id):
-    return redis_client.scard(user_id)
+    def expire_user_session_after(self, user_id, expire_second=3600):
+        # expire the user session after expire_second second, default to 1 hour
+        # Set expiry only when the key has no expiry
+        if self.redis_client.ttl(user_id) == -1:
+            self.redis_client.expire(user_id, expire_second)
 
-def get_books_from_redis(redis_client, user_id):
-    """
-    Get the list of books for the given user ID.
-    Returns an empty list if no books are found.
-    """
-    return redis_client.smembers(user_id)
+    def add_book(self, user_id, book):
+        """
+        Add a book to Redis for the given user ID.
+        Returns the number of books added, or 0 if the user ID is not provided.
+        """
+        if not user_id:
+            return 0
+        else:
+            return self.redis_client.sadd(user_id, book)
+
+    def add_books(self, user_id, book_list):
+        """
+        Add a list of books to Redis for the given user ID.
+        Returns the number of books added, or 0 if the user ID is not provided.
+        """
+        if not user_id:
+            return 0
+        else:
+            return self.redis_client.sadd(user_id, *book_list)
+
+    def get_num_books(self, user_id):
+        # Get the number of books in Redis for the given user ID
+        return self.redis_client.scard(user_id)
+
+    def get_books(self, user_id):
+        """
+        Get the list of books for the given user ID.
+        Returns an empty list if no books are found.
+        """
+        book_list = self.redis_client.smembers(user_id)
+        if book_list:
+            return book_list
+        else:
+            return []
+
+    def remove_user(self, user_id):
+        # Remove the list of books for the given user ID from Redis.
+        return self.redis_client.delete(user_id)
     
-
-def remove_user_from_redis(redis_client, user_id):
-    # Remove the list of books for the given user ID from Redis.
-    redis_client.delete(user_id)
-
-def test():
-
-    # Generate a user ID
-    user_id = generate_user_id()
-    redis_client = connect_to_redis()
-
-    #redis_client.set(user_id, ",".join(book_list))
-    #book_list = redis_client.get(user_id)
-
-    # Add a list of books to Redis for a user
-    book_list = ["Book 1", "Book 2", "Book 3"]
-    add_books_to_redis(redis_client, user_id, book_list)
-
-    # Get the list of books from Redis for a user
-    books = get_books_from_redis(redis_client, user_id)
-    for book in books:
-        print(book)
-
-    # Remove the list of books from Redis for a user
-    remove_user_from_redis(redis_client, user_id)
-
-
-
+# .decode('utf-8')    
