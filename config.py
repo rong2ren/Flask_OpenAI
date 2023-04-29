68e86688f74fa7e1093e2a54e0c9422b22af6317
rong2ren <98350815+rong2ren@users.noreply.github.com>
2023-04-29 14:22:06 -0700

    clean up

--- config.py
+++ config.py
@@ -1,23 +1,35 @@
 #This file contains most of the configuration variables that the app needs.
-
-from flask import Flask
-import logging
 from dotenv import load_dotenv
 import os
+from flask import Flask
+import logging
 
 # Load environment variables from .env file
 load_dotenv()
 
-# Create the Flask application
-app = Flask(__name__)
-# NOTE: The secret key is used to cryptographically-sign the cookies used for storing
-#       the session identifier.
-# Flask-Session won't work without a secret key; it's important to set this to a random string of characters.
-app.secret_key = os.environ.get('FLASK_SECRET_KEY')
-# Enable Flask's debug mode if the DEBUG environment variable is set
-app.config['FLASK_DEBUG'] = os.environ.get('FLASK_DEBUG', False)
+class Config:
+    # Flask configuration
+    SECRET_KEY = os.getenv('FLASK_SECRET_KEY')
+    DEBUG = False
 
-# Initialize logger - will use Flask's built-in logger
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+config_by_name = dict(
+    dev=DevelopmentConfig,
+    prod=ProductionConfig
+)
+config_name = os.getenv('APP_ENV', 'dev')
+
+app = Flask(__name__)
+app.config.from_object(config_by_name[config_name])
+
+
+# Logging configuration
 LOG_LEVEL_MAP = {
     'DEBUG': logging.DEBUG,
     'INFO': logging.INFO,
@@ -25,14 +37,21 @@ LOG_LEVEL_MAP = {
     'ERROR': logging.ERROR,
     'CRITICAL': logging.CRITICAL
 }
+LOGGING_LEVEL = os.getenv('LOGGING_LEVEL', 'INFO')
+"""
+1. The defulat logging format for flask is:
+%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]
+2. The default logging level for Flask logger is WARNING, 
+which means that only warning messages and above (error and critical) will be logged.
 
+Or you can config by
 logging.basicConfig(
-    level=LOG_LEVEL_MAP.get(os.getenv('LOGGING_LEVEL'), logging.INFO),
+    level=LOG_LEVEL_MAP.get(LOGGING_LEVEL, logging.INFO),
     format = '%(asctime)s - %(levelname)s - %(message)s'
-    )
+)
+"""
 logger = app.logger
-"""
-# 2nd option: create a new logger
+logger.setLevel(LOG_LEVEL_MAP.get(LOGGING_LEVEL, logging.WARNING))
 
-logger = logging.getLogger(__name__)
-"""
+
+
